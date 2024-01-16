c01002775ff9761bc4d228e9ad7528f6ac62468b
Joseph LeBlanc <4141762+jsleblanc@users.noreply.github.com>
2024-01-15 18:48:27 -0800

    starting to implement basic parsers; tests

--- UnitTests/Tests.fs
+++ UnitTests/Tests.fs
@@ -3,6 +3,83 @@ module Tests
 open System
 open Xunit
 
+open assembler.parsers
+open assembler.types
+open FParsec
+    
+[<Theory>]
+[<InlineData("//comment")>]
+[<InlineData("// comment")>]
+[<InlineData("//  comment")>]
+[<InlineData("//   comment ")>]
+let ``Should Parse Comments`` s =
+    let success = 
+        match run pComment s with
+        | Success _ -> true
+        | _ -> false
+    Assert.True(success)
+    
+[<Theory>]
+[<InlineData("not a comment")>]
+let ``Should Not Parse as Comments`` s =
+    let success = 
+        match run pComment s with
+        | Success _ -> false
+        | _ -> true
+    Assert.True(success)      
+   
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``Should Parse Jump - JGT`` () =
+    let success =
+        match run pJump "JGT" with
+        | Success(JGT, _, _) -> true
+        | _ -> false
+    Assert.True(success)
+    
+[<Fact>]
+let ``Should Parse Jump - JEQ`` () =
+    let success =
+        match run pJump "JEQ" with
+        | Success(JEQ, _, _) -> true
+        | _ -> false
+    Assert.True(success)
+    
+[<Fact>]
+let ``Should Parse Jump - JGE`` () =
+    let success =
+        match run pJump "JGE" with
+        | Success(JGE, _, _) -> true
+        | _ -> false
+    Assert.True(success)
+    
+[<Fact>]
+let ``Should Parse Jump - JLT`` () =
+    let success =
+        match run pJump "JLT" with
+        | Success(JLT, _, _) -> true
+        | _ -> false
+    Assert.True(success)
+    
+[<Fact>]
+let ``Should Parse Jump - JNE`` () =
+    let success =
+        match run pJump "JNE" with
+        | Success(JNE, _, _) -> true
+        | _ -> false
+    Assert.True(success)
+    
+[<Fact>]
+let ``Should Parse Jump - JLE`` () =
+    let success =
+        match run pJump "JLE" with
+        | Success(JLE, _, _) -> true
+        | _ -> false
+    Assert.True(success)
+    
+[<Fact>]
+let ``Should Parse Jump - JMP`` () =
+    let success =
+        match run pJump "JMP" with
+        | Success(JMP, _, _) -> true
+        | _ -> false
+    Assert.True(success)    
--- assembler/parsers.fs
+++ assembler/parsers.fs
@@ -1,5 +1,26 @@
 module assembler.parsers
 
 open FParsec
+open assembler.types
 
-let parseComment s = pstring "//" >>. pstring s .>> newline
+let ws = spaces // skips any whitespace
+
+let str s = pstring s
+let str_ws s = pstring s >>. ws
+
+let pComment = str_ws "//" >>. skipRestOfLine true
+
+let pSpacing   = // literal translation:
+                 //  skipManyChars (pSpace <|> pComment)
+                 // more efficient:
+                     skipSepBy spaces pComment
+let pJGT = str "JGT" >>. pSpacing |>> function _ -> JGT
+let pJEQ = str "JEQ" >>. pSpacing |>> function _ -> JEQ
+let pJGE = str "JGE" >>. pSpacing |>> function _ -> JGE
+let pJLT = str "JLT" >>. pSpacing |>> function _ -> JLT
+let pJNE = str "JNE" >>. pSpacing |>> function _ -> JNE
+let pJLE = str "JLE" >>. pSpacing |>> function _ -> JLE
+let pJMP = str "JMP" >>. pSpacing |>> function _ -> JMP
+let pJump = choice [pJGT; pJEQ; pJGE; pJLT; pJNE; pJLE; pJMP]
+
+let runpComment s = run pComment s
