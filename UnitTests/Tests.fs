9408cc13ae54414dc12a381aa262eeaaeecdbd7e
Joseph LeBlanc <4141762+jsleblanc@users.noreply.github.com>
2024-01-16 22:27:49 -0800

    tweaks

--- UnitTests/Tests.fs
+++ UnitTests/Tests.fs
@@ -114,11 +114,10 @@ let ``Should Not Parse as Jump`` s =
 [<InlineData("@1234", 1234)>]
 [<InlineData("@1", 1)>]
 let ``Should Parse as Constant`` s exp =
-    let success =
-        match run pConstant s with
-        | Success(Constant c, _, _) -> exp = c
-        | _ -> false
-    Assert.True(success)
+    match run pConstant s with
+    | Success(Constant c, _, _) -> Assert.Equal(exp, c)
+    | Failure(msg, _, _) -> Assert.Fail(msg)
+    | _ -> Assert.Fail("Parsing failed")
 
 [<Theory>]
 [<InlineData("1")>]
@@ -146,7 +145,7 @@ let ``Should Not Parse as Constant`` s =
 [<InlineData("@MYSCREEN", "MYSCREEN")>]
 let ``Should Parse as Variable`` s exp =
     match run pVariable s with
-    | Success(Variable v, _, _) -> Assert.Equal(v, exp)
+    | Success(Variable v, _, _) -> Assert.Equal(exp, v)
     | _ -> Assert.Fail("Parse failed")
     
 [<Theory>]
@@ -173,7 +172,7 @@ let ``Should Not Parse as Variable`` s =
 [<InlineData("(MYSCREEN)", "MYSCREEN")>]
 let ``Should Parse as Label`` s exp =
     match run pLabel s with
-    | Success(Label l, _, _) -> Assert.Equal(l, exp)
+    | Success(Label l, _, _) -> Assert.Equal(exp, l)
     | _ -> Assert.Fail("Parse failed")
     
 [<Theory>]
@@ -187,50 +186,50 @@ let ``Should Not Parse as Label`` s =
 [<Fact>]
 let ``Should Parse as Destination - M`` () =
     match run pDestination "M=" with
-    | Success (d, _, _) -> Assert.Equal(d, Destination.M)
+    | Success (d, _, _) -> Assert.Equal(Destination.M, d)
     | _ -> Assert.Fail("Parsing failed")
     
 [<Fact>]
 let ``Should Parse as Destination - D`` () =
     match run pDestination "D=" with
-    | Success (d, _, _) -> Assert.Equal(d, Destination.D)
+    | Success (d, _, _) -> Assert.Equal(Destination.D, d)
     | _ -> Assert.Fail("Parsing failed")
     
 [<Fact>]
 let ``Should Parse as Destination - DM`` () =
     match run pDestination "DM=" with
-    | Success (d, _, _) -> Assert.Equal(d, Destination.D ||| Destination.M)
+    | Success (d, _, _) -> Assert.Equal(Destination.D ||| Destination.M, d)
     | _ -> Assert.Fail("Parsing failed")
     
 [<Fact>]
 let ``Should Parse as Destination - A`` () =
     match run pDestination "A=" with
-    | Success (d, _, _) -> Assert.Equal(d, Destination.A)
+    | Success (d, _, _) -> Assert.Equal(Destination.A, d)
     | _ -> Assert.Fail("Parsing failed")
     
 [<Fact>]
 let ``Should Parse as Destination - AM`` () =
     match run pDestination "AM=" with
-    | Success (d, _, _) -> Assert.Equal(d, Destination.A ||| Destination.M)
+    | Success (d, _, _) -> Assert.Equal(Destination.A ||| Destination.M, d)
     | _ -> Assert.Fail("Parsing failed")
     
 [<Fact>]
 let ``Should Parse as Destination - AD`` () =
     match run pDestination "AD=" with
-    | Success (d, _, _) -> Assert.Equal(d, Destination.A ||| Destination.D)
+    | Success (d, _, _) -> Assert.Equal(Destination.A ||| Destination.D, d)
     | _ -> Assert.Fail("Parsing failed")    
     
 [<Fact>]
 let ``Should Parse as Destination - AMD`` () =
     match run pDestination "AMD=" with
-    | Success (d, _, _) -> Assert.Equal(d, Destination.A ||| Destination.M ||| Destination.D)
+    | Success (d, _, _) -> Assert.Equal(Destination.A ||| Destination.M ||| Destination.D, d)
     | _ -> Assert.Fail("Parsing failed") 
     
 [<Fact>]
 let ``Should Parse as Destination, Order does not matter - AMD, DAM`` () =
     let t s = 
         match run pDestination s with
-        | Success (d, _, _) -> Assert.Equal(d, Destination.A ||| Destination.M ||| Destination.D)
+        | Success (d, _, _) -> Assert.Equal(Destination.A ||| Destination.M ||| Destination.D, d)
         | _ -> Assert.Fail("Parsing failed")
     t "AMD="
     t "DAM="
@@ -279,34 +278,34 @@ let ``Should Not Parse as Destination`` s =
 [<InlineData("D|M", "D|M")>]
 let ``Should Parse as Computation`` s exp =
     match run pComputation s with
-    | Success(c, _, _) -> Assert.Equal(c, exp)
+    | Success(c, _, _) -> Assert.Equal(exp, c)
     | _ -> Assert.Fail("Parsing failed")
     
 [<Fact>]
 let ``Should Parse as Symbol - Variable``  () =
     match run pSymbol "@ITSR0" with
-    | Success(Variable s, _, _) -> Assert.Equal(s, "ITSR0")
+    | Success(Variable s, _, _) -> Assert.Equal("ITSR0", s)
     | Failure(msg, _, _) -> Assert.Fail(msg)
     | _ -> Assert.Fail("Parsing failed")
     
 [<Fact>]
 let ``Should Parse as Symbol - Predefined Symbol``  () =
     match run pSymbol "@R0" with
-    | Success(Predefined s, _, _) -> Assert.Equal(s, R0)
+    | Success(Predefined s, _, _) -> Assert.Equal(R0, s)
     | Failure(msg, _, _) -> Assert.Fail(msg)
     | _ -> Assert.Fail("Parsing failed")    
     
 [<Fact>]
 let ``Should Parse as Symbol - Constant``  () =
     match run pSymbol "@100" with
-    | Success(Constant s, _, _) -> Assert.Equal(s, uint16 100)
+    | Success(Constant s, _, _) -> Assert.Equal(uint16 100, s)
     | Failure(msg, _, _) -> Assert.Fail(msg)
     | _ -> Assert.Fail("Parsing failed")
     
 [<Fact>]
 let ``Should Parse as Symbol - Label``  () =
     match run pSymbol "(test)" with
-    | Success(Label s, _, _) -> Assert.Equal(s, "test")
+    | Success(Label s, _, _) -> Assert.Equal("test", s)
     | Failure(msg, _, _) -> Assert.Fail(msg)
     | _ -> Assert.Fail("Parsing failed")
     
@@ -318,6 +317,44 @@ let ``Should Parse as Symbol - Label``  () =
 [<InlineData("AM=M+1", "M+1")>]
 let ``Should Parse as C Instruction`` s exp =
     match run pCInstruction s with
-    | Success(C_Instruction (d, c, j), _, _) -> Assert.Equal(c, exp)
+    | Success(C_Instruction (d, c, j), _, _) -> Assert.Equal(exp, c)
     | Failure(msg, _, _) -> Assert.Fail(msg)
     | _ -> Assert.Fail("Parsing failed")
+    
+[<Fact>]
+let ``Should Parse Multiline Input`` () =
+    let s = """
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/06/max/Max.asm
+    
+// Computes R2 = max(R0, R1)  (R0,R1,R2 refer to RAM[0],RAM[1],RAM[2])
+
+   // D = R0 - R1
+   @R0
+   D=M
+   @R1
+   D=D-M
+   // If (D > 0) goto ITSR0
+   @ITSR0
+   D;JGT
+   // Its R1
+   @R1
+   D=M
+   @R2
+   M=D
+   @END
+   0;JMP
+(ITSR0)
+   @R0             
+   D=M
+   @R2
+   M=D
+(END)
+   @END
+   0;JMP
+"""
+    match run pAssembly s with
+    | Success(p, _, _) -> Assert.Equal(20, p.Length)
+    | Failure(msg, _, _) -> Assert.Fail(msg)
