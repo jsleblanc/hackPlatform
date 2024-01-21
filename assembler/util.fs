55dc1926fc88ad2ce425e8d17b2e514949ac186b
Joseph LeBlanc <4141762+jsleblanc@users.noreply.github.com>
2024-01-21 10:13:59 -0800

    symbol table dump in hexadecimal

--- assembler/util.fs
+++ assembler/util.fs
@@ -3,5 +3,5 @@ module assembler.util
 open System.IO
 
 let dumpSymbolsToDisk (map:Map<string, uint16>) fileName =
-    let lines = map |> Map.toList |> List.map (fun (k,v) -> $"{k},{v},") 
+    let lines = map |> Map.toList |> List.map (fun (k,v) -> $"{k},{v:X4},") 
     File.WriteAllLines(fileName, lines)
