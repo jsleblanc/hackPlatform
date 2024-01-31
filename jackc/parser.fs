d1ee4761884c8cafcd2600234dca809d31401301
Joseph LeBlanc <4141762+jsleblanc@users.noreply.github.com>
2024-01-31 13:14:21 -0800

    added libraries

--- jackc/parser.fs
+++ jackc/parser.fs
@@ -1,2 +1,8 @@
 module jackc.parser
 
+open FParsec
+
+let ws = spaces // skips any whitespace
+let str s = pstring s
+
+let pCommentSingleLine = str "//" >>. ws >>. restOfLine true //|>> function c -> Comment c
