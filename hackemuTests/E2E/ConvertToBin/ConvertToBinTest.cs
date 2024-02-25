03b7e9282543d3904099dfd07043fc7bd73a7e96
Joseph LeBlanc <4141762+jsleblanc@users.noreply.github.com>
2024-02-24 18:34:12 -0800

    extra test case

--- hackemuTests/E2E/ConvertToBin/ConvertToBinTest.cs
+++ hackemuTests/E2E/ConvertToBin/ConvertToBinTest.cs
@@ -36,5 +36,33 @@ public class ConvertToBinTest
             hackComputer.Memory(x).Should().Be(1, $"Memory address: {x}");
         }
         hackComputer.Memory(8016).Should().Be(0, "Memory address: 8016");
-    }    
+    }   
+    
+    [Fact]
+    public void ConvertToBinProgramTest_0x5555()
+    {
+        var binaryCode =
+            CompilerWorkflow.CompileFiles(new[] { new FileInfo("E2E/ConvertToBin/Main.jack") });
+        
+        var hackComputer = new HackComputer(binaryCode);
+        hackComputer.SetMemory(8000, 0x5555);
+        hackComputer.ComputeCycles(1_000_000);
+        
+        hackComputer.Memory(8001).Should().Be(1, "Memory address: 8001");
+        hackComputer.Memory(8002).Should().Be(0, "Memory address: 8002");
+        hackComputer.Memory(8003).Should().Be(1, "Memory address: 8003");
+        hackComputer.Memory(8004).Should().Be(0, "Memory address: 8004");
+        hackComputer.Memory(8005).Should().Be(1, "Memory address: 8005");
+        hackComputer.Memory(8006).Should().Be(0, "Memory address: 8006");
+        hackComputer.Memory(8007).Should().Be(1, "Memory address: 8007");
+        hackComputer.Memory(8008).Should().Be(0, "Memory address: 8008");
+        hackComputer.Memory(8009).Should().Be(1, "Memory address: 8009");
+        hackComputer.Memory(8010).Should().Be(0, "Memory address: 8010");
+        hackComputer.Memory(8011).Should().Be(1, "Memory address: 8011");
+        hackComputer.Memory(8012).Should().Be(0, "Memory address: 8012");
+        hackComputer.Memory(8013).Should().Be(1, "Memory address: 8013");
+        hackComputer.Memory(8014).Should().Be(0, "Memory address: 8014");
+        hackComputer.Memory(8015).Should().Be(1, "Memory address: 8015");
+        hackComputer.Memory(8016).Should().Be(0, "Memory address: 8016");
+    }        
 }
